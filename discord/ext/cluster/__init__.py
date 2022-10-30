5f5c5d690476a45c712989f8a0298684c178d32e
chredeur <50155089+chredeur0@users.noreply.github.com>
2022-10-30 23:06:45 +0100

    Update __init__.py

--- discord/ext/cluster/__init__.py
+++ discord/ext/cluster/__init__.py
@@ -5,14 +5,14 @@ Better Cluster
 A high-performance inter-process communication library designed 
 to handle communication between multiple shards
 
-:copyright: (c) 2022-present DaPandaOfficial
+:copyright: (c) 2022-present DaPandaOfficial remade by chredeur
 :license: MIT, see LICENSE for more details.
 
 """
 
-__version__ = "1.0.0"
-__title__ = "better-cluster"
-__author__ = "DaPandaOfficial"
+__version__ = "2.0"
+__title__ = "better-cluster-fastapi"
+__author__ = "DaPandaOfficial and chredeur"
 
 from .client import Client
 from .shard import Shard
